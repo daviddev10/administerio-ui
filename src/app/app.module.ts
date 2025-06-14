e425dada7cbb808ae99962edf98f3fe923e2d66b
David <d.david.ar10@gmail.com>
2025-06-14 02:17:55 -0400

    Agregado de primer componente y configuración de tema Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { RouterLink } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotfoundComponent
   ],
   imports: [
+    RouterLink,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // Angular Material
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
